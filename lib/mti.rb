9af08abc64972228c72e2323591ab33460435954
Kingsley Simon <b0yfunky4all86@gmail.com>
2019-01-30 17:04:00 +0800

    ref

--- lib/mti.rb
+++ lib/mti.rb
@@ -2,30 +2,61 @@ module Mti
   extend ActiveSupport::Concern
 
   included do
+    attr_accessor :mti_models
 
-    def self.has_subclass table
-      mattr_accessor :mti_models unless defined? self.mti_models
-      self.mti_models ||= []
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    def self.has_subclass(table, can_autosave)
+      @mti_models ||= []
+
       table_columns = table.to_s.camelize.constantize.column_names
                       .reject {|col| ["id", "#{self.to_s.underscore}_id"].include? col }
                       .compact
 
-      self.mti_models << table
+      @mti_models << table
 
-      has_one table, autosave: true, dependent: :destroy
+      has_one table, autosave: can_autosave, dependent: :destroy
 
       table_columns.each do |col|
         delegate col.to_sym, "#{col}=".to_sym, to: table, allow_nil: true
       end
     end
 
-    def initialize(attributes = nil)
-      super
-      self.mti_models.map {|model|
-        mti_model_column = model.to_s.camelize.constantize.column_names 
-        self.send("build_#{model.to_s}", attributes.to_h.reject {|key, val| val unless mti_model_column.include? key.to_s}) 
-      } if defined? self.mti_models
+    def self.create(attributes = nil, options = {}, &block)
+      byebug
+      if attributes.is_a?(Array)
+        attributes.collect { |attr| create(attr, options, &block) }
+      else
+        object = new(attributes, options)
+        @mti_models.map {|model|
+          mti_model_column = model.to_s.camelize.constantize.column_names
+          object.send("build_#{model.to_s}", attributes.reject {|key, val| val unless mti_model_column.include? key.to_s})
+        }
+        yield(object) if block_given?
+        object.save
+        object
+      end
     end
   end
 end
